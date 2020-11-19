55a772bd872ac45764290788c006c55bdca518d0
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-11-19 16:20:10 +0900

    07

--- main.c
+++ main.c
@@ -5,24 +5,34 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	char filename[20];
+	char target[20];
+	char str[80];
+	char *res;
 	FILE *fp;
-	char c;
-	int i;
+	int n;
 	
-	fp = fopen("sample.txt", "r");
-	if (fp == NULL) {
-		printf("파일을 열지 못했습니다.\n");
-		return 1;
+	printf("original file name : ");
+	scanf("%s", filename);
+	printf("word to find : ");
+	scanf("%s", target);
+	
+	fp = fopen(filename, "r");
+	n = strlen(target);
+	
+	while (1) {
+		res = fgets(str, sizeof(str), fp);
+		if (res == NULL) {
+			break;
+		}
+		else if (strncmp(res, target, n) == 0) {
+			printf("Find a word \"%s\"\n", target);
+			printf("Search done!\n");
+			break;
+		}
 	}
-	else {
-		printf("파일 열기에 성공하였습니다.\n");
-	}
 	
-	while ((c=fgetc(fp)) != EOF) {
-		putchar(c);
-	}	
-	
-	fclose(fp);
+	fclose(fp);	
 	
 	return 0;
 }
