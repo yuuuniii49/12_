ea515788f0d19c64aefac5506771587100a98a2e
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-11-19 15:23:09 +0900

    02

--- main.c
+++ main.c
@@ -5,11 +5,19 @@
 
 int main(int argc, char *argv[]) {
 	
-	char code1 = 'A';
-	int code2 = 65;
+	int i = 0;
+	char str[4];
 	
-	printf("code1 = %c (%d)\n", code1, code1);
-	printf("code2 = %c (%d)\n", code2, code2);
+	str[0] = 'a';
+	str[1] = 'b';
+	str[2] = 'c';
+	str[3] = '\0';
 	
+	printf("%s\n", str);
+	
+	while (str[i] != '\0') {
+		printf("%c", str[i]);
+		i++;
+	}
 	return 0;
 }
